5434677995f48d8d7f481e98ce8654fdd58a151a
Nina <nina888nina888@mail.ru>
2021-03-18 23:16:48 +0100

    ооло

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
               id="checkbox3"
               type="checkbox"
               class="checkbox3 visuallyHidden"
-            >
+            />
             <label for="checkbox3">
               <div
                 v-if="mobilWive"
@@ -24,10 +24,7 @@
             </label>
           </nav>
 
-          <div
-            class="cont"
-            :class="{ open: showNav }"
-          >
+          <div class="cont" :class="{ open: showNav }">
             <div class="header__wrap">
               <navbar v-if="!mobilWive" />
             </div>
@@ -35,31 +32,29 @@
         </div>
       </header>
       <div class="bg">
-        <div class="h1">
-          Gemütlichkeit im Haus macht uns glücklicher
-        </div>
+        <div class="h1">Gemütlichkeit im Haus macht uns glücklicher</div>
       </div>
       <mainwin />
       <sect />
       <cartsect />
       <tabs />
       <section-firm />
-      <footer />
+      <footer-v />
     </body>
   </div>
 </template>
 <script>
-import Navbar from './components/navbar.vue'
-import Mainwin from './components/mainwin.vue'
-import Sect from './components/sectionwin.vue'
-import Cartsect from './components/cartsect.vue'
-import Tabs from './components/tabs.vue'
-import SectionFirm from './components/section-firm'
-import Footer from './components/footer'
-import NovigationMobile from './components/novigation-mobile'
+import Navbar from "./components/navbar.vue";
+import Mainwin from "./components/mainwin.vue";
+import Sect from "./components/sectionwin.vue";
+import Cartsect from "./components/cartsect.vue";
+import Tabs from "./components/tabs.vue";
+import SectionFirm from "./components/section-firm";
+import NovigationMobile from "./components/novigation-mobile";
+import FooterV from "./components/footer-v";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Navbar,
     Mainwin,
@@ -67,25 +62,25 @@ export default {
     Tabs,
     Cartsect,
     SectionFirm,
-    Footer,
-    NovigationMobile
+    NovigationMobile,
+    FooterV,
   },
   data: () => {
     return {
       mobilWive: true,
-      showNav: false
-    }
+      showNav: false,
+    };
   },
-  created () {
-    this.handLeView()
-    window.addEventListener('resize', this.handLeView)
+  created() {
+    this.handLeView();
+    window.addEventListener("resize", this.handLeView);
   },
   methods: {
-    handLeView () {
-      this.mobilWive = window.innerWidth <= 1272
-    }
-  }
-}
+    handLeView() {
+      this.mobilWive = window.innerWidth <= 1272;
+    },
+  },
+};
 </script>
 <style lang="scss">
 * {
@@ -99,7 +94,7 @@ body {
 }
 .bg__burg {
   background-color: #076550;
-  }
+}
 .h1 {
   font-family: "Noto Serif TC", serif;
   font-style: normal;
@@ -117,29 +112,29 @@ body {
 
   color: #ffff;
 
- &__wrap {
+  &__wrap {
     max-height: 100%;
-   }
+  }
 }
 .burger__menu {
-  cursor:pointer;
-   z-index: 55;
-     margin: 0 auto;
-    margin-top: 30px;
-    width: 40px;
-    height: 40px;
-    position: relative;
+  cursor: pointer;
+  z-index: 55;
+  margin: 0 auto;
+  margin-top: 30px;
+  width: 40px;
+  height: 40px;
+  position: relative;
 }
 .burger__line {
-    padding: 0;
-    width: 70px;
-    height: 7px;
-    background-color: rgb(236, 228, 228);
-    display: block;
-    border-radius: 4px;
-    transition: all 0.4s ease-in-out;
-    position: absolute;
-    right:20px
+  padding: 0;
+  width: 70px;
+  height: 7px;
+  background-color: rgb(236, 228, 228);
+  display: block;
+  border-radius: 4px;
+  transition: all 0.4s ease-in-out;
+  position: absolute;
+  right: 20px;
 }
 
 .top_burger {
@@ -152,23 +147,17 @@ body {
   position: relative;
 }
 .burger__line1 {
-
-     top: 0px;
+  top: 0px;
 }
 .burger__line2,
-.burger__line3{
-
+.burger__line3 {
   top: 17px;
 }
 .burger__line3 {
-
-     right: 20px;
-
+  right: 20px;
 }
 .burger__line4 {
-
-       bottom: 0px;
-
+  bottom: 0px;
 }
 .burger__menu3 .burger__line1 {
   transform-origin: 9%;
@@ -176,39 +165,41 @@ body {
 .burger__menu3 .burger__line4 {
   transform-origin: 9%;
 }
-.checkbox3:checked + label > .burger__menu3 > .burger__line1{
-    transform: rotate(45deg);
-    height: 7px;
-    width: 58px;
+.checkbox3:checked + label > .burger__menu3 > .burger__line1 {
+  transform: rotate(45deg);
+  height: 7px;
+  width: 58px;
 }
 
-.checkbox3:checked + label >.burger__menu3 > .burger__line3{
-    transform: rotate(45deg);
-    height: 7px;
-    background-color: transparent;
+.checkbox3:checked + label > .burger__menu3 > .burger__line3 {
+  transform: rotate(45deg);
+  height: 7px;
+  background-color: transparent;
 }
 
-.checkbox3:checked + label >.burger__menu3 > .burger__line2{
-    transform: rotate(-45deg);
-    height: 7px;
-    background-color: transparent;
+.checkbox3:checked + label > .burger__menu3 > .burger__line2 {
+  transform: rotate(-45deg);
+  height: 7px;
+  background-color: transparent;
 }
 
-.checkbox3:checked + label >.burger__menu3 > .burger__line4{
-    transform: rotate(-45deg);
-    height: 7px;
-    width: 58px;
+.checkbox3:checked + label > .burger__menu3 > .burger__line4 {
+  transform: rotate(-45deg);
+  height: 7px;
+  width: 58px;
 }
 .visuallyHidden {
-    position: absolute;
-    overflow: hidden;
-    clip: rect(0 0 0 0);
-    height: 1px; width: 1px;
-    margin: -1px; padding: 0; border: 0;
+  position: absolute;
+  overflow: hidden;
+  clip: rect(0 0 0 0);
+  height: 1px;
+  width: 1px;
+  margin: -1px;
+  padding: 0;
+  border: 0;
 }
 .cont {
   z-index: 14;
-
 }
 
 .open {
@@ -217,64 +208,75 @@ body {
   z-index: 8;
 }
 
-@media(max-width:1272px){
+@media (max-width: 1272px) {
   .bg__burg {
-  background-color: #076550;
-  padding-bottom: 40px;
+    background-color: #076550;
+    padding-bottom: 40px;
+  }
 }
+@media (max-width: 1168px) {
+  .h1 {
+    font-size: 40px;
+  }
 }
-@media(max-width:1168px){
-  .h1{font-size: 40px;}
-}
-@media(max-width:1168px){
-  .h1{font-size: 40px;}
+@media (max-width: 1168px) {
+  .h1 {
+    font-size: 40px;
+  }
 }
 
 // lap
-@media(max-width:768px){
-  .h1{
+@media (max-width: 768px) {
+  .h1 {
     font-weight: 500;
-font-size: 36px;
-line-height: 54px;
-letter-spacing: 0.11em;}
-.burger__line {
+    font-size: 36px;
+    line-height: 54px;
+    letter-spacing: 0.11em;
+  }
+  .burger__line {
     width: 52px;
     height: 6px;
-     right:10px
-}
-}
-@media(max-width:677px){
-  .h1{font-weight: normal;
-font-size: 36px;
-line-height: 46px;}
-}
-@media(max-width:558px){
-  .h1{font-weight: normal;
-font-size: 33px;
-line-height: 46px;}
-}
-@media(max-width:400px){
-  .h1{font-weight: normal;
-font-size: 25px;
-line-height: 36px;}
-.burger__line {
-    width: 42px;
-    height: 6px;
-}
-.top_burger{
-  padding-right: 10px;
-}
-}
-@media(max-width:344px){
-  .h1{
-font-size: 24px;
+    right: 10px;
   }
 }
-@media(max-width:330px){
-  .h1{font-weight: normal;
-font-size: 18px;
-line-height: 26px;}
-
+@media (max-width: 677px) {
+  .h1 {
+    font-weight: normal;
+    font-size: 36px;
+    line-height: 46px;
+  }
 }
-
+@media (max-width: 558px) {
+  .h1 {
+    font-weight: normal;
+    font-size: 33px;
+    line-height: 46px;
+  }
+}
+@media (max-width: 400px) {
+  .h1 {
+    font-weight: normal;
+    font-size: 25px;
+    line-height: 36px;
+  }
+  .burger__line {
+    width: 42px;
+    height: 6px;
+  }
+  .top_burger {
+    padding-right: 10px;
+  }
+}
+@media (max-width: 344px) {
+  .h1 {
+    font-size: 24px;
+  }
+}
+@media (max-width: 330px) {
+  .h1 {
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 26px;
+  }
+}
 </style>
---
+++ src/components/footer-v.vue
@@ -0,0 +1,79 @@
+<template>
+  <footer id="footer">
+    <div class="footer__wr">
+      <div class="footer__wr_logo">
+    <div class="logo">LOGO</div>
+    <p class="logo__text"><span>Logo</span>macht das Zuhause gemütlich</p>
+    <address>Munsterschtrasse 34 Essen 34533</address>
+    <p class="tel">
+      Tel:<a href="tel:4848 940 123 23 33 0" class="tel"
+        >4848 940 123 23 33 0</a
+      >
+    </p>
+    <div class="wrap__ciz">
+      <img src="" alt="" />
+      <img src="" alt="" />
+      <img src="" alt="" />
+    </div>
+      </div>
+     <div class="wrap__titl_but">
+    <div class="titel">Lezte News</div>
+
+    <div class="wrap_but">
+      <button type="button">click</button>
+      <p class="button_text">Sie können uns in Düsseldorf kontaktieren</p>
+    </div>
+    <div class="wrap_but">
+      <button type="button">click</button>
+      <p class="button_text">Sie können uns in Düsseldorf kontaktieren</p>
+    </div>
+    </div>
+    <div class="footer__list__wrap">
+    <div class="titel">Seitennavigation</div>
+    <ul class="footer__list">
+      <li><a href="">Neu</a></li>
+      <li><a href="">Westwing Collection</a></li>
+      <li><a href="">Möbel</a></li>
+      <li><a href="">Deko</a></li>
+      <li><a href="">Leuchten</a></li>
+    </ul>
+    </div>
+     <div class="footer__list__wrap">
+    <div class="titel">Seitennavigation</div>
+    <ul class="footer__list">
+      <li><a href="">Leuchten</a></li>
+      <li><a href="">maecenas</a></li>
+      <li><a href="">Tisch & Bar</a></li>
+      <li><a href="">Kinderzimmer</a></li>
+      <li><a href="">Gutscheine</a></li>
+    </ul>
+    </div>
+    </div>
+  </footer>
+</template>
+<script>
+
+</script>
+<style lang="scss">
+.footer__wr{
+  background-color: #F4DEDF;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.wrap__titl_but{
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+}
+.wrap_but{
+  display: flex;
+}
+.footer__list__wrap{
+    display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+}
+</style>
